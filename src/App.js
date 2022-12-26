a3bb09c866beaab403a48d6d7fec6eca619a3d30
stephenprin <stephenprince427@gmail.com>
2022-12-26 11:58:01 +0100

    setting up the router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Error from './pages/Error';
+import SharedLayout from './pages/SharedLayout';
+import Products from './pages/Products';
+import SingleProducts from './pages/SingleProduct';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+    <Routes>
+    <Route path='/' element={<SharedLayout />}>
+      <Route index element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='dashboard' element={<Dashboard />} />
+      <Route path='products' element={<Products />} />
+      <Route path='products/productId' element={<SingleProducts />} />
+      <Route path='*' element={<Error />} />
+    </Route>
+  </Routes>
+ </Router>
+  )
 }
 
 export default App;
